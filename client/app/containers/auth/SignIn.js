921a4a6c3a6997e3c320b979981ffac423cb6350
Passion Dev <az.fullstack2258@gmail.com>
2019-09-03 19:33:30 -0700

    Add signup, signin pages

---
+++ client/app/containers/auth/SignIn.js
@@ -0,0 +1,125 @@
+import React from 'react';
+import {
+  Avatar,
+  Button,
+  CssBaseline,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  Link,
+  Grid,
+  Container,
+  Typography,
+} from '@material-ui/core';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import { withStyles } from '@material-ui/core/styles';
+import { auto } from 'html-webpack-plugin/lib/chunksorter';
+
+const Copyright = () => (
+  <Typography variant="body2" color="textSecondary" align="center">
+    {'Copyright © '}
+    <Link color="inherit" href="/">
+      Your Website
+    </Link>{' '}
+    {new Date().getFullYear()}
+    {'.'}
+  </Typography>
+);
+
+const styles = theme => ({
+  '@global': {
+    body: {
+      backgroundColor: theme.palette.common.white,
+    },
+  },
+  container: {
+    maxWidth: 1240,
+    margin: 'auto',
+    padding: 20,
+  },
+  paper: {
+    marginTop: theme.spacing.unit,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing.unit,
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing.unit,
+  },
+  submit: {
+    margin: theme.spacing.unit,
+  },
+});
+
+const SignIn = ({ classes }) => {
+  return (
+    <div className={classes.container}>
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="/signup" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      {Copyright()}
+    </div>
+  );
+};
+
+export default withStyles(styles)(SignIn);
--- client/app/index.js
+++ client/app/index.js
@@ -1,27 +1,25 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  Switch
-} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
-import App from './components/App/App';
-import NotFound from './components/App/NotFound';
+import App from './components/app/App';
+import NotFound from './components/app/NotFound';
 
-import HelloWorld from './components/HelloWorld/HelloWorld';
+import SignIn from './containers/auth/SignIn';
+import SignUp from './containers/auth/SignUp';
 
 import './styles/styles.scss';
 
-render((
+render(
   <Router>
     <App>
       <Switch>
-        <Route exact path="/" component={HelloWorld}/>
-        <Route component={NotFound}/>
+        <Route exact path="/login" component={SignIn} />
+        <Route exact path="/signup" component={SignUp} />
+        <Route component={NotFound} />
       </Switch>
     </App>
-  </Router>
-), document.getElementById('app'));
+  </Router>,
+  document.getElementById('app'),
+);
